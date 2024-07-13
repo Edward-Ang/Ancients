5d1be693cb89f5e4160375bc28c6d102b638312e
Edward Ang <wei.hen.ang@gmail.com>
2024-07-13 19:02:02 +0800

    hide apikey

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
-import styles from "./page.module.css";
 import Header from "@/components/header/header";
 import Main from "@/components/main/main";
 
 async function getData() {
-  const response = await fetch('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e51073a1a8724915ac0d1622c70b114a');
-  const data = await response.json();
-  return data;
+  const apiKey = process.env.NEWS_API_KEY;
+  try {
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${apiKey}`, {
+/*       next: { revalidate: 60 }, // Optional: for caching and revalidation */
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return { articles: [] }; // Return empty data on error
+  }
 }
 
 export default async function Home() {
@@ -17,4 +27,3 @@ export default async function Home() {
     </>
   );
 }
-
