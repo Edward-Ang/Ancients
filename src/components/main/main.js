07ffd7564fe1db282585c31cd7ce642c09618eaf
Edward Ang <wei.hen.ang@gmail.com>
2024-07-17 15:12:27 +0800

    + side content

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,21 +4,51 @@ import { RocketLaunch } from "@mui/icons-material";
 import './main.css';
 
 const categories = [
-    "world",
-    "regional",
-    "politics",
-    "finance",
-    "academia",
-    "programming",
-    "lifestyle",
-    "food",
-    "opinion",
-    "game"
+    {
+        name: "world",
+        color: "mojito",
+    },
+    {
+        name: "regional",
+        color: "mojito", 
+    },
+    {
+        name: "politics",
+        color: "malibu",
+    },
+    {
+        name: "finance",
+        color: "malibu",
+    },
+    {
+        name: "academia",
+        color: "lime",
+    },
+    {
+        name: "programming",
+        color: "lime",
+    },
+    {
+        name: "lifestyle",
+        color: "azure",
+    },
+    {
+        name: "food",
+        color: "azure",
+    },
+    {
+        name: "opinion",
+        color: "peach",
+    },
+    {
+        name: "game",
+        color: "peach",
+    }
 ];
 
 export default function Main({ data }) {
     const articlesToShow = data.news.filter((article) =>
-        article.description !== null && article.url !== 'https://removed.com'
+        article.description !== "mojito" && article.url !== 'https://removed.com' && article.image !== 'None'
     );
 
     return (
@@ -61,13 +91,16 @@ export default function Main({ data }) {
                         <RocketLaunch className="rocket-icon" />
                         <span className="source-title">Other Topics</span>
                     </div>
-                    {categories.map((topic) => (
-                        <div key={topic.id} className="source-card">
-                            <Link href={`/category?category=${topic}`} className="source-name">
-                                {topic.charAt(0).toUpperCase() + topic.slice(1)}
+                    <div className="topic-container">
+                        {categories.map((topic, index) => (
+                            <Link key={index}
+                                href={`/category?category=${topic.name}`}
+                                className="source-name"
+                                style={{ background: `var(--${topic.color})` }}>
+                                <span>{topic.name.charAt(0).toUpperCase() + topic.name.slice(1)}</span>
                             </Link>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
