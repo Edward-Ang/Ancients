03b34f290ad66d8d3e08cf01532d9bc74bd98ef3
Edward Ang <wei.hen.ang@gmail.com>
2024-07-18 15:06:06 +0800

    + google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Roboto } from "next/font/google";
+import Script from "next/Script";
 import "./globals.css";
 
 const roboto = Roboto({
@@ -8,13 +9,23 @@ const roboto = Roboto({
 
 export const metadata = {
   title: "Ancients News",
-  description: "Discover the latest news and updates from around the world on Ancients News. Stay informed with breaking headlines, in-depth articles, and diverse perspectives. Explore trending topics in politics, technology, entertainment, and more, curated for your interests. Visit now for real-time news coverage and analysis.",
+  deScription: "Discover the latest news and updates from around the world on Ancients News. Stay informed with breaking headlines, in-depth articles, and diverse perspectives. Explore trending topics in politics, technology, entertainment, and more, curated for your interests. Visit now for real-time news coverage and analysis.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="end">
       <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-B41W5P4D5Y"></Script>
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-B41W5P4D5Y');          
+          `}
+        </Script>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
