a8185df853956841c44ebb1d933f709589fc077b
Edward Ang <wei.hen.ang@gmail.com>
2024-07-26 14:25:48 +0800

    chg sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,37 +1,37 @@
 export default function sitemap() {
     return [
         {
-            url: 'https://ancientsnews.com',
+            url: 'https://www.ancientsnews.com',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-            url: 'https://ancientsnews.com/technology',
+            url: 'https://www.ancientsnews.com/technology',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.8,
         },
         {
-            url: 'https://ancientsnews.com/business',
+            url: 'https://www.ancientsnews.com/business',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.7,
         },
         {
-            url: 'https://ancientsnews.com/entertainment',
+            url: 'https://www.ancientsnews.com/entertainment',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.6,
         },
         {
-            url: 'https://ancientsnews.com/sports',
+            url: 'https://www.ancientsnews.com/sports',
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.5,
         },
         {
-            url: 'https://ancientsnews.com/about',
+            url: 'https://www.ancientsnews.com/about',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.4,
